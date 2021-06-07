1e211312df69d5f0bea5174d755f9d53400c2e10
MD SABBIR KHAN <64284482+md-sabbir-khan@users.noreply.github.com>
2021-06-07 09:14:19 -0700

    last

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 $x = "Hello php!";
 $y = 'i am from openshift!';
 $z = " i am added after ";
+$zz = " i am added after last ";
 
 echo $x;
 echo "<br>"; 
 echo $y;
+echo "<br>";
 echo $z;
+echo "<br>";
+echo $zz;
 ?>
